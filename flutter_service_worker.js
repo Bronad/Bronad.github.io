c38610ea62200bae494e2ed22dbad91aebfb2d29
Bronad <90397301+Bronad@users.noreply.github.com>
2023-04-15 20:50:46 +0200

    try

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -58,9 +58,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "8d7731a2dc7f96541ef6518ebec26cf0",
-"/": "8d7731a2dc7f96541ef6518ebec26cf0",
-"main.dart.js": "a291574cb80ef8677f243e543ac615d5",
+"index.html": "c4df9ea61f67afb6c7888102f1b6740f",
+"/": "c4df9ea61f67afb6c7888102f1b6740f",
+"main.dart.js": "610ba8bb778af24c55ab76c4c25f80f7",
 "manifest.json": "e8a86cdd0e8f0de9b40f37f215b3abc3",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
